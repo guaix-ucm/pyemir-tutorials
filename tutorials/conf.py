5377765ca505bd9477d67bd01f14043aa5f0f0cd
Sergio Pascual <sergiopr@fis.ucm.es>
2020-07-12 18:10:17 +0200

    Add prev/next links

--- tutorials/conf.py
+++ tutorials/conf.py
@@ -56,3 +56,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'show_related': True,
+    'show_relbar_bottom': True,
+    'show_relbar_top': False
+}
+
